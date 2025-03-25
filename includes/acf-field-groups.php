be7920193b45f31a3bd2997befe37218c467c9e8
atc <birdhive@gmail.com>
2025-03-25 14:56:34 -0400

    cont.

--- includes/acf-field-groups.php
+++ includes/acf-field-groups.php
@@ -3926,7 +3926,7 @@ function mlib_register_field_groups() {
 				'required' => 0,
 				'conditional_logic' => 0,
 				'wrapper' => array(
-					'width' => '25',
+					'width' => '20',
 					'class' => '',
 					'id' => '',
 				),
