4e492bb7151c5ea8f530ea8bae03360c8ec8c4e4
atc-mba25 <birdhive@gmail.com>
2025-12-09 13:07:52 -0500

    Cleanup/removing dependency on SDG -- wip

--- mlib.php
+++ mlib.php
@@ -4,8 +4,8 @@
  * Description:       A WordPress plugin for managing a Music Library (Works/Editions) using ACF PRO Blocks, Post Types, Options Pages, Taxonomies and more.
  * //Requires at least: 6.4
  * //Requires PHP:      7.4
- * Dependencies:      Requires WHx4 plugin for People CPT and SDG for various utility functions
- * Requires Plugins:  whx4, sdg
+ * Dependencies:      Requires WHx4 plugin for People CPT
+ * Requires Plugins:  whx4
  * Version:           0.1
  * Author:            atc
  * License:           GPL-2.0-or-later
@@ -28,51 +28,12 @@ if ( !function_exists( 'add_action' ) ) {
     exit;
 }
 
-// Enforce dependency on WHx4
-/*add_action('plugins_loaded', function() {
-    if ( !class_exists('atc\WHx4\Plugin') ) {
-        add_action('admin_notices', function() {
-            echo '<div class="notice notice-error"><p><strong>MLib</strong> requires the <strong>WHx4</strong> plugin to be active. The plugin has been deactivated.</p></div>';
-        });
-
-        add_action('admin_init', function() {
-            deactivate_plugins(plugin_basename(__FILE__));
-        });
-
-        return;
-    }
-});*/
-
-// WIP >> OOP
-/*
-// Via Composer
-require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
-
-use atc\MLib\Modules\Repertoire;
-use atc\MLib\Modules\Instruments;
-use atc\MLib\Modules\Builders;
-use atc\MLib\Modules\Organs;
-*/
-/*add_filter( 'whx4_register_modules', function( array $modules ) {
-    //$modules['music'] = Music::class;
-    $modules = [
-        //'repertoire'    => Repertoire::class, // or: Music?
-        //'instruments'    => Instruments::class,
-           //'builder'        => Builders::class,
-        //'organs'         => Organs::class // tmp?
-    ];
-    return $modules;
-});*/
-
-
 // Define our handy constants.
 define( 'MLIB_VERSION', '0.1.5' );
 define( 'MLIB_PLUGIN_DIR', __DIR__ );
 define( 'MLIB_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'MLIB_PLUGIN_BLOCKS', MLIB_PLUGIN_DIR . '/blocks/' );
 //$plugin_path = plugin_dir_path( __FILE__ );
-
-/* +~+~+ *** +~+~+ */
 
 /* +~+~+ ACF +~+~+ */
 
@@ -90,12 +51,6 @@ require 'includes/acf-field-groups.php';
 
 // Post types, taxonomies, field groups
 require 'includes/cpts.php';
-
-// Load custom post types
-//require 'posttypes.php';
-
-// Load custom taxonomies
-//require 'taxonomies.php';
 
 /* +~+~+ Optional Modules +~+~+ */
 
@@ -160,7 +115,8 @@ foreach ( $active_modules as $module ) {
 add_action( 'template_redirect', 'acf_form_head' ); // See https://wordpress.org/support/topic/acf-create-a-front-end-form/
 
 add_shortcode('mlib_acf_form', 'mlib_acf_form');
-function mlib_acf_form ( $atts = array() ) {
+function mlib_acf_form ( $atts = array() ) 
+{
 
     $info = "";
     $ts_info = "";
@@ -197,14 +153,3 @@ function mlib_acf_form ( $atts = array() ) {
     return $info;
 
 }
-
-/*
-// WIP -- attempt to prevent conversion to (and addition of) curly quotes in front-end forms
-// This didn't work -- apparently theme-related -- switched back to Trudy and problem went away
-function my_acf_remove_curly_quotes() {
-    remove_filter ('acf_the_content', 'wptexturize');
-}
-add_action('acf/init', 'my_acf_remove_curly_quotes');
-*/
-
-?>
